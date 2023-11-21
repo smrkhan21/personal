f510c492efcfd6e71932fdc9b6c558d39a9e08f3
Sameer Khan <sameerpathan1997@gmail.com>
2023-11-21 14:29:11 +0530

    Changed position

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -13,7 +13,7 @@ const Profile = () => {
       <div className="section__text">
         <p className="section__text__p1">Hello, I'm</p>
         <h1 className="title">Sameer Khan</h1>
-        <p className="section__text__p2">Frontend Developer</p>
+        <p className="section__text__p2">Software Developer</p>
         <div className="btn-container">
           <button
             className="btn btn-color-2"
